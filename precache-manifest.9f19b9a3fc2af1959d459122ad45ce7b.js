e4ba9be7014b186727f307abd692eede56245637
RamiB1234 <rbisthebest@hotmail.com>
2020-07-14 04:14:06 +0300

    Updates

---
+++ precache-manifest.9f19b9a3fc2af1959d459122ad45ce7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5223d56eb0b3a81af131e525fe394",
+    "url": "/resturant-front/index.html"
+  },
+  {
+    "revision": "6586abe00c5bca30ec80",
+    "url": "/resturant-front/static/css/2.fea45d51.chunk.css"
+  },
+  {
+    "revision": "6bce87b251f4410fe5f1",
+    "url": "/resturant-front/static/css/main.e3f4d514.chunk.css"
+  },
+  {
+    "revision": "6586abe00c5bca30ec80",
+    "url": "/resturant-front/static/js/2.92dc60e7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/resturant-front/static/js/2.92dc60e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bce87b251f4410fe5f1",
+    "url": "/resturant-front/static/js/main.641cfaba.chunk.js"
+  },
+  {
+    "revision": "79ed437d22afc15da9a0",
+    "url": "/resturant-front/static/js/runtime-main.82ac21f0.js"
+  }
+]);
